8055c083438adb4222b57b26fb8d0a4204b04d0c
hotephen <ghcks1000@gmail.com>
2020-02-03 00:45:37 +0900

    commit

--- tacker/nfvo/nfvo_plugin.py
+++ tacker/nfvo/nfvo_plugin.py
@@ -383,7 +383,7 @@ class NfvoPlugin(nfvo_db_plugin.NfvoPluginDb, vnffg_db.VnffgPluginDbMixin,
                                               'scale_chain',
                                               chain_id= '45df1c99-530b-4d9a-930d-e6003cebdd91' ,
                                               vnf = vnf,
-                                              symmetrical=sfc['symmetrical'])
+                                              auth_attr=vim_obj['auth_cred'])
         LOG.info("scaling_chain 2")
                                             
         return updated_ppg
