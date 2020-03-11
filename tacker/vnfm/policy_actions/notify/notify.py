64960389ddca51b764d75050fb1c05e9c480bff1
hotephen <ghcks1000@gmail.com>
2020-03-11 14:23:36 +0900

    commit

--- tacker/vnfm/policy_actions/notify/notify.py
+++ tacker/vnfm/policy_actions/notify/notify.py
@@ -20,6 +20,9 @@ from tacker.vnfm import utils as vnfm_utils
 from tacker import manager
 from tacker.common import rpc
 from tacker.common import topics
+from tacker.conductor.conductorrpc import AutoHealingRPC
+from tacker import context as t_context
+
 
 LOG = logging.getLogger(__name__)
 
@@ -52,24 +55,47 @@ class VNFActionNotify(abstract_action.AbstractPolicyAction):
 #        vnf_new_id = vnf_old_id
 #        nfvo_plugin.mark_event(context, vnf_old_id,vnf_new_id)
 
-        def start_rpc_listeners():
-            self.endpoints = [self]
-            self.connection = rpc.create_connection()
-            LOG.info('log: self.connection = %s', self.connection) ###
-            self.connection.create_consumer(topics.TOPIC_ACTION_KILL,
-                                            self.endpoints, fanout=False,
-                                            host=vnf_old_id)
-            LOG.info('log: create_consumer completed') ###
-            return self.connection.consume_in_threads()
-
+        # Start rpc connection
         try:
             rpc.init_action_rpc(cfg.CONF) ###
-            LOG.info('log: dir(cfg.CONF) = %s', dir(cfg.CONF)) ###
             servers = start_rpc_listeners()
-
         except Exception:
             LOG.exception('failed to start rpc')
             return 'FAILED'
 
-        
+        # Call 'vnf_respawning_evnt' method via RPC
+        try:
+            target = AutoHealingRPC.AutoHealingRPC.target
+            rpc_client = rpc.get_client(target)
+            LOG.info('log: rpc_client = %s', rpc_client) ###
+            LOG.info('log: rpc_server = %s', rpc.get_server(target, self.endpoints)) ###
+            
+            cctxt = rpc_client.prepare()
+            LOG.info('log: cctxt = %s', cctxt) ###
+            status = cctxt.call(t_context.get_admin_context_without_session(),
+                                'vnf_respawning_event', #conductor_server.vnf_respawning_event
+                                vnf_id=vnf_id) #
+            LOG.info('log: status = %s', status) ###
 
+        # Stop rpc connection
+        for server in servers:
+            try:
+                server.stop()
+            except Exception:
+                LOG.exception(
+                    'failed to stop rpc connection for vnf %s',
+                    vnf_id)
+    
+
+        def start_rpc_listeners():
+            self.endpoints = [self]
+            self.connection = rpc.create_connection()
+            self.connection.create_consumer(topics.TOPIC_ACTION_KILL,
+                                            self.endpoints, fanout=False,
+                                            host=vnf_old_id)
+
+            LOG.info('log: self.endpoints = %s', self.endpoints) ###
+            LOG.info('log: self.connection = %s', self.connection) ###
+            LOG.info('log: create_consumer completed') ###
+
+            return self.connection.consume_in_threads()
